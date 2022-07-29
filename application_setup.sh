dfb9da0060b9278d7610e3a8a8a0c118ff267f90
Adefemi Micheal <adefemi171@gmail.com>
2022-07-29 21:46:38 +0100

    Update application_setup.sh

--- application_setup.sh
+++ application_setup.sh
@@ -6,7 +6,21 @@ sudo apt update && sudo apt upgrade -y
 
 
 echo "Install Node.js"
-sudo apt install nodejs -y 
+d ~
+curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs -y
+
+rm nodesource_setup.sh
+
+
+## You may also need development tools to build native addons:
+sudo apt-get install gcc g++ make -y
+## To install the Yarn package manager, run:
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn -y
+
 
 echo "Install Nginx/apache"
 sudo apt install nginx -y
